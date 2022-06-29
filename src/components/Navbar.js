5821300ff9a4f4bcb1c2b189e489f3c66303e5df
Marcos8060 <marcosgav80@gmail.com>
2022-06-29 11:57:01 +0300

    used ternary operator for conditional rendering in the navbar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React,{useContext} from "react";
 import { HiMenuAlt1 } from 'react-icons/hi'
 import { BiLogIn } from 'react-icons/bi'
 import { SiGnuprivacyguard } from 'react-icons/si'
 import { Link } from "react-router-dom";
 import '../assets/css/navbar.css'
+import { userContext } from "../context/AuthContext";
+import { HiMenuAlt3 } from 'react-icons/hi'
 
 
 const Navbar = () => {
+  const { user } = useContext(userContext)
+
   return (
     <>
       <nav className="navbar navbar-expand-lg">
@@ -23,10 +27,18 @@ const Navbar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <HiMenuAlt3 className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav ms-auto">
+            <ul className="navbar-nav mx-auto">
+            {
+                user && 
+                <li className="nav-item">
+                  <h4 className="welcome">welcome <span className="username">{user.username}</span></h4>
+              </li>
+              }
+            </ul>
+            <ul className="navbar-nav ms-auto">              
               <li className="nav-item">
                 <Link to='/about' className="nav-link active" aria-current="page">
                   About
