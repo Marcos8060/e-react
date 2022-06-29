7c3910559b23cc6b6de4f9918fd3792544befc17
Marcos8060 <marcosgav80@gmail.com>
2022-06-29 08:33:12 +0300

    created the hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import hero from '../assets/images/hero.png'
+import '../assets/css/hero.css'
+import { Link } from 'react-router-dom'
+
+const Hero = () => {
+  return (
+    <div className='container'>
+        <div className="row hero">
+            <div className="col-md-6">
+                <img className='img-fluid heroImg' src={hero} alt="" />
+            </div>
+            <div className="col-md-6">
+                <h1 className='heroTitle'>Get to <span className='yellow'>hire</span> some of the most professional housemaids in <span className='yellow'>Nairobi</span>.</h1>
+                <Link to='/about' className='heroBtn btn'>Get started</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
